f6b339d41c24d7b854a81d2e46cec90ec1a4092d
Calvin Zachman <calvin.zachman@protonmail.com>
2024-06-05 15:20:57 -0500

    temp: add new RPC error codes
    These will be used by the SendOnion RPC to communicate information
    about HTLCSwitch errors to RPC callers. Remote instantiations of the
    ChannelRouter will rely on error information via RPC to reconstruct
    error types needed during error handling.
    
    NOTE: This may move to an existing package eventually. Also, maybe
    there is a better way to do this?

---
+++ errorcodes/errorcodes.go
@@ -0,0 +1,10 @@
+package errorcodes
+
+const (
+	ErrCodePaymentIDNotFound        = "PaymentIDNotFound"
+	ErrCodeUnreadableFailureMessage = "UnreadableFailureMessage"
+	ErrCodeSwitchExiting            = "SwitchExiting"
+	ErrCodeNoLink                   = "NoLink"
+	ErrCodeInvalidArgument          = "InvalidArgument"
+	ErrCodeInternal                 = "Internal"
+)
