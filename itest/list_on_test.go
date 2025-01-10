2fd0b4d2fcd239fea50774dbfbebbead17c0cab7
Calvin Zachman <calvin.zachman@protonmail.com>
2025-01-09 18:49:36 -0600

    itest: add duplicate onion send test
    This demonstrates how the Switch and SendOnion rpc
    behave when asked to dispatch duplicate onions. Notably,
    the Switch circuit map detects this - but only if the
    matching onion is still in flight. Once the circuit is
    torn down, the duplicate is permitted by the Switch.
    
    It is likely that we will add a layer of protection to the
    SendOnion call itself to prevent duplicates even after the
    first HTLC is no longer in-flight.
    
    TODO: Determine whether this SendOnion duplication protection
    should presist across restarts.

--- itest/list_on_test.go
+++ itest/list_on_test.go
@@ -708,6 +708,10 @@ var allTestCases = []*lntest.TestCase{
 		TestFunc: testSendOnion,
 	},
 	{
+		Name:     "send onion twice",
+		TestFunc: testSendOnionTwice,
+	},
+	{
 		Name:     "track onion",
 		TestFunc: testTrackOnion,
 	},
