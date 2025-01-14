b646e3279886cac2f4da64e837d52991aefd6953
Calvin Zachman <calvin.zachman@protonmail.com>
2025-01-14 09:31:24 -0600

    lntest: add switchrpc methods
    Add FetchAttemptResults and DeleteAttemptResult to harness.

--- lntest/rpc/switch.go
+++ lntest/rpc/switch.go
@@ -54,3 +54,33 @@ func (h *HarnessRPC) BuildOnion(
 
 	return resp
 }
+
+// FetchAttemptResults makes a RPC call to FetchAttemptResults and asserts.
+//
+//nolint:lll
+func (h *HarnessRPC) FetchAttemptResults(
+	req *switchrpc.FetchAttemptResultsRequest) *switchrpc.FetchAttemptResultsResponse {
+
+	ctxt, cancel := context.WithTimeout(h.runCtx, DefaultTimeout)
+	defer cancel()
+
+	resp, err := h.Switch.FetchAttemptResults(ctxt, req)
+	h.NoError(err, "FetchAttemptResult")
+
+	return resp
+}
+
+// DeleteAttemptResult makes a RPC call to DeleteAttemptResult and asserts.
+//
+//nolint:lll
+func (h *HarnessRPC) DeleteAttemptResult(
+	req *switchrpc.DeleteAttemptResultRequest) *switchrpc.DeleteAttemptResultResponse {
+
+	ctxt, cancel := context.WithTimeout(h.runCtx, DefaultTimeout)
+	defer cancel()
+
+	resp, err := h.Switch.DeleteAttemptResult(ctxt, req)
+	h.NoError(err, "DeleteAttemptResult")
+
+	return resp
+}
