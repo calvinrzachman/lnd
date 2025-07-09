0ca813185e21f3aea3a63794dd1091b6fde6da50
Calvin Zachman <calvin.zachman@protonmail.com>
2025-07-09 13:57:15 -0400

    switchrpc: add NextAttemptID proto

--- lnrpc/switchrpc/switch_grpc.pb.go
+++ lnrpc/switchrpc/switch_grpc.pb.go
@@ -27,6 +27,10 @@ type SwitchClient interface {
 	TrackOnion(ctx context.Context, in *TrackOnionRequest, opts ...grpc.CallOption) (*TrackOnionResponse, error)
 	// BuildOnion attempts to build an onion packet for the specified route.
 	BuildOnion(ctx context.Context, in *BuildOnionRequest, opts ...grpc.CallOption) (*BuildOnionResponse, error)
+	// NextAttemptID returns a new unique attempt ID for use in a SendOnion call.
+	// The caller may optionally specify a namespace to ensure non-colliding
+	// IDs in multi-client scenarios.
+	NextAttemptID(ctx context.Context, in *NextAttemptIDRequest, opts ...grpc.CallOption) (*NextAttemptIDResponse, error)
 }
 
 type switchClient struct {
@@ -64,6 +68,15 @@ func (c *switchClient) BuildOnion(ctx context.Context, in *BuildOnionRequest, op
 	return out, nil
 }
 
+func (c *switchClient) NextAttemptID(ctx context.Context, in *NextAttemptIDRequest, opts ...grpc.CallOption) (*NextAttemptIDResponse, error) {
+	out := new(NextAttemptIDResponse)
+	err := c.cc.Invoke(ctx, "/switchrpc.Switch/NextAttemptID", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // SwitchServer is the server API for Switch service.
 // All implementations must embed UnimplementedSwitchServer
 // for forward compatibility
@@ -77,6 +90,10 @@ type SwitchServer interface {
 	TrackOnion(context.Context, *TrackOnionRequest) (*TrackOnionResponse, error)
 	// BuildOnion attempts to build an onion packet for the specified route.
 	BuildOnion(context.Context, *BuildOnionRequest) (*BuildOnionResponse, error)
+	// NextAttemptID returns a new unique attempt ID for use in a SendOnion call.
+	// The caller may optionally specify a namespace to ensure non-colliding
+	// IDs in multi-client scenarios.
+	NextAttemptID(context.Context, *NextAttemptIDRequest) (*NextAttemptIDResponse, error)
 	mustEmbedUnimplementedSwitchServer()
 }
 
@@ -92,6 +109,9 @@ func (UnimplementedSwitchServer) TrackOnion(context.Context, *TrackOnionRequest)
 }
 func (UnimplementedSwitchServer) BuildOnion(context.Context, *BuildOnionRequest) (*BuildOnionResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method BuildOnion not implemented")
+}
+func (UnimplementedSwitchServer) NextAttemptID(context.Context, *NextAttemptIDRequest) (*NextAttemptIDResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method NextAttemptID not implemented")
 }
 func (UnimplementedSwitchServer) mustEmbedUnimplementedSwitchServer() {}
 
@@ -160,6 +180,24 @@ func _Switch_BuildOnion_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Switch_NextAttemptID_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(NextAttemptIDRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SwitchServer).NextAttemptID(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/switchrpc.Switch/NextAttemptID",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SwitchServer).NextAttemptID(ctx, req.(*NextAttemptIDRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // Switch_ServiceDesc is the grpc.ServiceDesc for Switch service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -178,6 +216,10 @@ var Switch_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "BuildOnion",
 			Handler:    _Switch_BuildOnion_Handler,
+		},
+		{
+			MethodName: "NextAttemptID",
+			Handler:    _Switch_NextAttemptID_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
