9c586e45efeecd773049a863c53da5fa3e221270
Calvin Zachman <calvin.zachman@protonmail.com>
2025-09-19 08:36:05 -0500

    htlcswitch: prevent router collision
    Prevent silent loss of htlc attempt information
    and resulting stuck payments via introduction of a
    db flag which indicates whether lnd has been used with
    a remote router (switchrpc) configuration.
    
    The switch will refuse to start if lnd is built without
    the switchrpc server while this db flag is present.
    
    The safety check during Switch creation prevents start
    up of the Switch if it contains attempt data from use
    with a remote router. This avoids lnd's onboard channel
    router from clearing state needed to track payment
    life-cycle for payments that it did not inititate.
    
    NOTE: This may be unnecessary once the Switch stores
    are namespaced and can be shared by multiple routers
    at once.

--- htlcswitch/payment_result.go
+++ htlcswitch/payment_result.go
@@ -42,6 +42,15 @@ var (
 	ErrAmbiguousAttemptInit = errors.New(
 		"ambiguous result for payment attempt registration",
 	)
+
+	// remoteRouterMarkerBucket is a bucket that contains the marker
+	// key used to indicate that the htlcswitch's payment attempt
+	// history is being managed by an external router.
+	remoteRouterMarkerBucket = []byte("remote-router-marker-bucket")
+
+	// remoteRouterMarkerKey is the key that will be present in the
+	// marker bucket if the switch is remote-managed.
+	remoteRouterMarkerKey = []byte("remote-router-marker-key")
 )
 
 // DeletionStatus represents the outcome of deleting a single attempt from
@@ -129,6 +138,9 @@ func deserializeNetworkResult(r io.Reader) (*networkResult, error) {
 type networkResultStore struct {
 	backend kvdb.Backend
 
+	// isRemote indicates that the store is being used in remote mode.
+	isRemote bool
+
 	// results is a map from paymentIDs to channels where subscribers to
 	// payment results will be notified.
 	results    map[uint64][]chan *networkResult
@@ -141,12 +153,68 @@ type networkResultStore struct {
 	attemptIDMtx *multimutex.Mutex[uint64]
 }
 
-func newNetworkResultStore(db kvdb.Backend) *networkResultStore {
+func newNetworkResultStore(db kvdb.Backend,
+	isRemote bool) (*networkResultStore, error) {
+
+	// Check for a state mismatch. If this is a local-managed router
+	// but the database has been marked for remote management, we must
+	// exit to prevent data loss.
+	if !isRemote {
+		var isMarkedRemote bool
+		err := db.View(func(tx kvdb.RTx) error {
+			bucket := tx.ReadBucket(remoteRouterMarkerBucket)
+			if bucket != nil &&
+				bucket.Get(remoteRouterMarkerKey) != nil {
+
+				isMarkedRemote = true
+			}
+
+			return nil
+		}, func() {
+			isMarkedRemote = false
+		})
+		if err != nil {
+			return nil, fmt.Errorf("unable to check for remote "+
+				"router marker: %w", err)
+		}
+
+		if isMarkedRemote {
+			return nil, fmt.Errorf("the htlc attempt database is " +
+				"marked for remote management by a switchrpc " +
+				"build, but this binary is in local " +
+				"management mode. Halting to prevent data " +
+				"loss. To use this database, restart with an " +
+				"lnd build that includes the `switchrpc` " +
+				"build tag")
+		}
+	}
+
+	// If we are in remote-managed mode, write the marker. This is
+	// placed after the check above to ensure we don't write and then
+	// immediately fail in a misconfigured dev environment.
+	if isRemote {
+		err := db.Update(func(tx kvdb.RwTx) error {
+			bucket, err := tx.CreateTopLevelBucket(
+				remoteRouterMarkerBucket,
+			)
+			if err != nil {
+				return err
+			}
+
+			return bucket.Put(remoteRouterMarkerKey, []byte{0x01})
+		}, func() {})
+		if err != nil {
+			return nil, fmt.Errorf("unable to write remote "+
+				"router marker: %w", err)
+		}
+	}
+
 	return &networkResultStore{
 		backend:      db,
+		isRemote:     isRemote,
 		results:      make(map[uint64][]chan *networkResult),
 		attemptIDMtx: multimutex.NewMutex[uint64](),
-	}
+	}, nil
 }
 
 // InitAttempt initializes the payment attempt with the given attemptID.
