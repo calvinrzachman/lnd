4af96767be4c892f6ec7bd96e558f3c2b3aa08f4
Calvin Zachman <calvin.zachman@protonmail.com>
2026-01-06 17:05:07 -0500

    switchrpc: slim error code enum
    Only include codes which help disambiguate grpc
    status codes which would otherwise be ambiguous
    due to overloaded meaning.
    
    Eg: codes.Unavailable may mean that the server
    was never reached. It may also occur when the
    server is shutting down. The inclusion of the
    SWITCH_EXITING error code can directly communicate
    a shutdown to a client.
    
    Likewise, a codes.Internal error may not be explict
    enought to indicate the critical case where the attempt
    initialization which offers duplicate protection
    itsefl fails. For this, we have AMBIGUOUS_STATE.

--- lnrpc/switchrpc/switch_server.go
+++ lnrpc/switchrpc/switch_server.go
@@ -884,6 +884,7 @@ func marshallSendOnionError(err error) error {
 		details.ClearTextFailure = &ClearTextFailure{
 			WireMessage: buf.Bytes(),
 		}
+		details.ErrorCode = ErrorCode_CLEAR_TEXT_ERROR
 		rpcCode = codes.FailedPrecondition
 
 	case errors.Is(err, htlcswitch.ErrUnreadableFailureMessage):
